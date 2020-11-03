1f0c17aded1d156e51089f1f977e0ab81a58c18f
DIEGO PEREIRA RAMOS DOS SANTOS <dieguitoo.2004@gmail.com>
2020-11-03 15:45:16 -0200

    Pulo do dino

--- Dino/js/jogo.js
+++ Dino/js/jogo.js
@@ -1,2 +1,43 @@
 // Jogo .js
 console.log('Início co Jogo!');
+
+const dino = document.querySelector('.dino');
+
+let dinoPosition = 0;
+let estaPulando = false;
+
+//Verificar se a tecla se a tecla espaço doi pressionada
+document.addEventListener('keydown' ,(event)=>{
+    if (event.code === 'Space')
+        if (!estaPulando) pular();
+});
+
+function pular(){
+    let intervaloPulo = setInterval (()=>{
+        estaPulando = true;
+        if (dinoPosition >= 250){
+            console.log('Topo!');
+            clearInterval(intervaloPulo);
+            let intervaloQueda = setInterval(()=>{
+                if (dinoPosition <= 0) {
+                    console.log('Chão');
+                    estaPulando = false;
+                    clearInterval(intervaloQueda);
+
+                } else {
+                    //Decrementar a posição do Dino
+                    dinoPosition -= 20;
+                    //Atualizar a posição na tela
+                    dino.style.bottom = dinoPosition + 'px';
+                }
+            },20);
+
+        } else {
+            //Incrementar a posição do Dino
+            dinoPosition += 20;
+            //Atualizar e posição na tela
+            dino.style.bottom = dinoPosition + 'px';
+        }
+
+    },20);
+}
